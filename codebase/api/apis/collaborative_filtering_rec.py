bbdc3c7c988dfdac141d5934d4c005a99a72f171
Baquara <Baquara@users.noreply.github.com>
2022-08-06 13:46:04 -0300

    Making the project up to date

--- codebase/api/apis/collaborative_filtering_rec.py
+++ codebase/api/apis/collaborative_filtering_rec.py
@@ -1,220 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import os
 import pandas as pd
-
-
-
-# In[2]:
-
-
-# read data
-df_items = pd.read_sql_table('items', 'postgres:///db_name',
-
-    usecols=['itemId', 'title'],
-    dtype={'itemId': 'int32', 'title': 'str'})
-
-df_ratings = pd.read_sql_table('ratings', 'postgres:///db_name',
-    usecols=['userId', 'itemId', 'rating'],
-    dtype={'userId': 'int32', 'itemId': 'int32', 'rating': 'float32'})
-
-
-# In[3]:
-
-
-df_items.head()
-df_items.shape
-
-
-# In[4]:
-
-
-df_ratings.head()
-
-
-# In[5]:
-
-
-# df_ratings.shape
-df_ratings=df_ratings[:2000000]
-
-
-# In[6]:
-
-
-df_ratings.shape
-
-
-# In[7]:
-
-
-from scipy.sparse import csr_matrix
-# pivot ratings into item features
-df_item_features = df_ratings.pivot(
-    index='itemId',
-    columns='userId',
-    values='rating'
-).fillna(0)
-
-
-# In[8]:
-
-
-mat_item_features = csr_matrix(df_item_features.values)
-
-
-# In[9]:
-
-
-df_item_features.head()
-
-
-# In[10]:
-
-
-from sklearn.neighbors import NearestNeighbors
-model_knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
-
-
-# In[11]:
-
-
-num_users = len(df_ratings.userId.unique())
-num_items = len(df_ratings.itemId.unique())
-print('There are {} unique users and {} unique items in this data set'.format(num_users, num_items))
-
-
-# In[12]:
-
-
-# get count
-df_ratings_cnt_tmp = pd.DataFrame(df_ratings.groupby('rating').size(), columns=['count'])
-df_ratings_cnt_tmp
-
-
-# In[13]:
-
-
-# there are a lot more counts in rating of zero
-total_cnt = num_users * num_items
-rating_zero_cnt = total_cnt - df_ratings.shape[0]
-
-df_ratings_cnt = df_ratings_cnt_tmp.append(
-    pd.DataFrame({'count': rating_zero_cnt}, index=[0.0]),
-    verify_integrity=True,
-).sort_index()
-df_ratings_cnt
-
-
-# In[14]:
-
-
-#log normalise to make it easier to interpret on a graph
-import numpy as np
-df_ratings_cnt['log_count'] = np.log(df_ratings_cnt['count'])
-df_ratings_cnt
-
-
-# In[15]:
-
-
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
-
-get_ipython().run_line_magic('matplotlib', 'inline')
-ax = df_ratings_cnt[['count']].reset_index().rename(columns={'index': 'rating score'}).plot(
-    x='rating score',
-    y='count',
-    kind='bar',
-    figsize=(12, 8),
-    title='Count for Each Rating Score (in Log Scale)',
-    logy=True,
-    fontsize=12,
-)
-ax.set_xlabel("item rating score")
-ax.set_ylabel("number of ratings")
-
-
-# In[16]:
-
-
-# get rating frequency
-#number of ratings each item got.
-df_items_cnt = pd.DataFrame(df_ratings.groupby('itemId').size(), columns=['count'])
-df_items_cnt.head()
-
-
-# In[17]:
-
-
-#now we need to take only items that have been rated atleast 50 times to get some idea of the reactions of users towards it
-
-popularity_thres = 50
-popular_items = list(set(df_items_cnt.query('count >= @popularity_thres').index))
-df_ratings_drop_items = df_ratings[df_ratings.itemId.isin(popular_items)]
-print('shape of original ratings data: ', df_ratings.shape)
-print('shape of ratings data after dropping unpopular items: ', df_ratings_drop_items.shape)
-
-
-# In[18]:
-
-
-# get number of ratings given by every user
-df_users_cnt = pd.DataFrame(df_ratings_drop_items.groupby('userId').size(), columns=['count'])
-df_users_cnt.head()
-
-
-# In[19]:
-
-
-# filter data to come to an approximation of user likings.
-ratings_thres = 50
-active_users = list(set(df_users_cnt.query('count >= @ratings_thres').index))
-df_ratings_drop_users = df_ratings_drop_items[df_ratings_drop_items.userId.isin(active_users)]
-print('shape of original ratings data: ', df_ratings.shape)
-print('shape of ratings data after dropping both unpopular items and inactive users: ', df_ratings_drop_users.shape)
-
-
-# In[20]:
-
-
-# pivot and create item-user matrix
-item_user_mat = df_ratings_drop_users.pivot(index='itemId', columns='userId', values='rating').fillna(0)
-#map item titles to images
-item_to_idx = {
-    item: i for i, item in 
-    enumerate(list(df_items.set_index('itemId').loc[item_user_mat.index].title))
-}
-# transform matrix to scipy sparse matrix
-item_user_mat_sparse = csr_matrix(item_user_mat.values)
-
-
-# In[21]:
-
-
-item_user_mat_sparse
-
-
-# In[22]:
-
-
-# define model
-model_knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
-# fit
-model_knn.fit(item_user_mat_sparse)
-
-
-# In[23]:
-
-
+from sqlalchemy import *
 from fuzzywuzzy import fuzz
+from sklearn.neighbors import NearestNeighbors
+from scipy.sparse import csr_matrix
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 
-
-# In[24]:
 
 
 def fuzzy_matching(mapper, fav_item, verbose=True):
@@ -224,9 +16,11 @@ def fuzzy_matching(mapper, fav_item, verbose=True):
     Parameters
     ----------    
     mapper: dict, map item title name to index of the item in data
+
     fav_item: str, name of user input item
     
     verbose: bool, print log if True
+
     Return
     ------
     index of the closest match
@@ -247,19 +41,24 @@ def fuzzy_matching(mapper, fav_item, verbose=True):
     return match_tuple[0][1]
 
 
-# In[25]:
 
-
-def make_recommendation(model_knn, data, mapper, fav_item, n_recommendations):
+def make_recommendation(model_knn, data, mapper, fav_item, n_recommendations,engine):
     """
     return top n similar item recommendations based on user's input item
+
+
     Parameters
     ----------
     model_knn: sklearn model, knn model
+
     data: item-user matrix
+
     mapper: dict, map item title name to index of the item in data
+
     fav_item: str, name of user input item
+
     n_recommendations: int, top n recommendations
+
     Return
     ------
     list of top n similar item recommendations
@@ -267,9 +66,9 @@ def make_recommendation(model_knn, data, mapper, fav_item, n_recommendations):
     # fit
     model_knn.fit(data)
     # get input item index
-    print('You have input item:', fav_item)
     idx = fuzzy_matching(mapper, fav_item, verbose=True)
-    
+    #idx = fav_item
+    print(" "+ str(idx) +" ")
     print('Recommendation system start to make inference')
     print('......\n')
     distances, indices = model_knn.kneighbors(data[idx], n_neighbors=n_recommendations+1)
@@ -277,29 +76,178 @@ def make_recommendation(model_knn, data, mapper, fav_item, n_recommendations):
     raw_recommends =         sorted(list(zip(indices.squeeze().tolist(), distances.squeeze().tolist())), key=lambda x: x[1])[:0:-1]
     # get reverse mapper
     reverse_mapper = {v: k for k, v in mapper.items()}
+    d = {'id': [], 'title': [], 'distance':[]}
     # print recommendations
+    output = ('Recommendations for {}:'.format(fav_item))
     print('Recommendations for {}:'.format(fav_item))
     for i, (idx, dist) in enumerate(raw_recommends):
+        d['id'].append(i+1)
+        d['title'].append(reverse_mapper[idx])
+        d['distance'].append(dist)
+        output = output + "<br>" + ('{0}: {1}, with distance of {2}'.format(i+1, reverse_mapper[idx], dist))
         print('{0}: {1}, with distance of {2}'.format(i+1, reverse_mapper[idx], dist))
+    df = pd.DataFrame(data=d)
+    df.to_sql('recommendations', con=engine, if_exists='replace')
+    return output
 
 
-# In[26]:
+def start():
+    start = time.perf_counter()
 
 
-my_favorite = 'Iron Man'
+    dataprocessstart = time.perf_counter()
+    engine = create_engine('sqlite:///./db.db?check_same_thread=False')
 
-make_recommendation(
-    model_knn=model_knn,
-    data=item_user_mat_sparse,
-    fav_item=my_favorite,
-    mapper=item_to_idx,
-    n_recommendations=10)
+    
 
 
-# In[27]:
+    # read data
+
+    df_items = pd.read_sql_query('''SELECT * FROM items''', con=engine)
+    df_ratings = pd.read_sql_query('''SELECT * FROM users''', con=engine)
 
 
-item_to_idx
+    df_items.head()
+    df_items.shape
 
 
-# In[ ]:
+    df_ratings.head()
+
+
+    # df_ratings.shape
+    df_ratings=df_ratings[:2000000]
+
+
+
+    df_ratings.shape
+
+
+    # pivot ratings into item features
+    df_item_features = df_ratings.pivot(
+        index='itemId',
+        columns='userId',
+        values='rating'
+    ).fillna(0)
+
+    dataprocessend = time.perf_counter() - dataprocessstart
+    recstart = time.perf_counter()
+    mat_item_features = csr_matrix(df_item_features.values)
+
+
+
+    df_item_features.head()
+
+    model_knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
+
+
+    num_users = len(df_ratings.userId.unique())
+    num_items = len(df_ratings.itemId.unique())
+    print('There are {} unique users and {} unique items in this data set'.format(num_users, num_items))
+
+
+    # get count
+    df_ratings_cnt_tmp = pd.DataFrame(df_ratings.groupby('rating').size(), columns=['count'])
+
+
+    # there are a lot more counts in rating of zero
+    total_cnt = num_users * num_items
+    rating_zero_cnt = total_cnt - df_ratings.shape[0]
+
+    df_ratings_cnt = df_ratings_cnt_tmp.append(
+        pd.DataFrame({'count': rating_zero_cnt}, index=[0.0]),
+        verify_integrity=True,
+    ).sort_index()
+
+
+
+
+    #log normalise to make it easier to interpret on a graph
+
+    df_ratings_cnt['log_count'] = np.log(df_ratings_cnt['count'])
+    df_ratings_cnt
+
+    plt.style.use('ggplot')
+
+    ax = df_ratings_cnt[['count']].reset_index().rename(columns={'index': 'rating score'}).plot(
+        x='rating score',
+        y='count',
+        kind='bar',
+        figsize=(12, 8),
+        title='Count for Each Rating Score (in Log Scale)',
+        logy=True,
+        fontsize=12,
+    )
+    ax.set_xlabel("item rating score")
+    ax.set_ylabel("number of ratings")
+
+
+
+    # get rating frequency
+    #number of ratings each item got.
+    df_items_cnt = pd.DataFrame(df_ratings.groupby('itemId').size(), columns=['count'])
+    df_items_cnt.head()
+
+
+
+    #now we need to take only items that have been rated atleast 50 times to get some idea of the reactions of users towards it
+
+    popularity_thres = 50
+    popular_items = list(set(df_items_cnt.query('count >= @popularity_thres').index))
+    df_ratings_drop_items = df_ratings[df_ratings.itemId.isin(popular_items)]
+    print('shape of original ratings data: ', df_ratings.shape)
+    print('shape of ratings data after dropping unpopular items: ', df_ratings_drop_items.shape)
+
+
+    # get number of ratings given by every user
+    df_users_cnt = pd.DataFrame(df_ratings_drop_items.groupby('userId').size(), columns=['count'])
+    df_users_cnt.head()
+
+
+
+    # filter data to come to an approximation of user likings.
+    ratings_thres = 50
+    active_users = list(set(df_users_cnt.query('count >= @ratings_thres').index))
+    df_ratings_drop_users = df_ratings_drop_items[df_ratings_drop_items.userId.isin(active_users)]
+    print('shape of original ratings data: ', df_ratings.shape)
+    print('shape of ratings data after dropping both unpopular items and inactive users: ', df_ratings_drop_users.shape)
+
+
+
+    # pivot and create item-user matrix
+    item_user_mat = df_ratings_drop_users.pivot(index='itemId', columns='userId', values='rating').fillna(0)
+    #map item titles to images
+    item_to_idx = {
+        item: i for i, item in 
+        enumerate(list(df_items.set_index('itemId').loc[item_user_mat.index].title))
+    }
+    # transform matrix to scipy sparse matrix
+    item_user_mat_sparse = csr_matrix(item_user_mat.values)
+
+
+    item_user_mat_sparse
+
+
+
+
+    # define model
+    model_knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
+    # fit
+    model_knn.fit(item_user_mat_sparse)
+
+
+    my_favorite = "Jumanji"
+
+    output = make_recommendation(
+        model_knn=model_knn,
+        data=item_user_mat_sparse,
+        fav_item=my_favorite,
+        mapper=item_to_idx,
+        n_recommendations=10,engine=engine)
+    recend = time.perf_counter() - recstart
+    end = time.perf_counter() - start
+    return "Total API endpoint execution time: "+str(end)+"<br>"+"Data processing time: "+str(dataprocessend)+"<br>"+"Recommendation execution time: "+str(recend)+"<br>"+output
+
+    #item_to_idx
+
+
+
--- codebase/api/apis/content_based_rec.py
+++ codebase/api/apis/content_based_rec.py
@@ -1,33 +1,51 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+from sqlalchemy import *
+import time
 
-ds = pd.read_sql_table('items', 'postgres:///db_name')  
 
-tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0, stop_words='english')
-tfidf_matrix = tf.fit_transform(ds['description'])
+def itemDesc(id,ds):
+    return ds.loc[ds['itemId'] == id]['description'].tolist()[0]
 
-cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
-
-results = {}
-
-for idx, row in ds.iterrows():
-    similar_indices = cosine_similarities[idx].argsort()[:-100:-1]
-    similar_items = [(cosine_similarities[idx][i], ds['id'][i]) for i in similar_indices]
-
-    results[row['id']] = similar_items[1:]
-    
-print('done!')
-
-def item(id):
-    return ds.loc[ds['id'] == id]['description'].tolist()[0].split(' - ')[0]
+def itemName(id,ds):
+    return ds.loc[ds['itemId'] == id]['title'].tolist()[0]
 
 # Just reads the results out of the dictionary.
-def recommend(item_id, num):
-    print("Recommending " + str(num) + " products similar to " + item(item_id) + "...")
-    print("-------")
+def recommend(ds,results,item_id, num):
+    end = "Recommending " + str(num) + " items similar to " + itemName(item_id,ds) + "..."
+    end = end + "<br>" + "-------"
     recs = results[item_id][:num]
     for rec in recs:
-        print("Recommended: " + item(rec[1]) + " (score:" + str(rec[0]) + ")")
+        end = end + "<br>" + "Recommended: " +"Name:  "+itemName(rec[1],ds)+ ";"+ itemDesc(rec[1],ds) + " (score:" + str(rec[0]) + ")"
+    return end
 
-recommend(item_id=11, num=5)
+
+def start(dbn,itid):
+    start = time.perf_counter()
+    dataprocessstart = time.perf_counter()
+    print('sqlite:///./db'+str(dbn)+'.db?check_same_thread=False')
+    engine = create_engine('sqlite:///./db'+str(dbn)+'.db?check_same_thread=False')
+
+    ds = pd.read_sql_query('''SELECT * FROM items''', con=engine)
+    dataprocessend = time.perf_counter() - dataprocessstart
+    recstart = time.perf_counter()
+
+    tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0, stop_words='english')
+    tfidf_matrix = tf.fit_transform(ds['description']+"|"+ds['tag'])
+
+    cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+    results = {}
+
+    for idx, row in ds.iterrows():
+        similar_indices = cosine_similarities[idx].argsort()[:-100:-1]
+        similar_items = [(cosine_similarities[idx][i], ds['itemId'][i]) for i in similar_indices]
+
+        results[row['itemId']] = similar_items[1:]
+
+    print('done!')
+    string = recommend(ds,results,item_id=itid, num=10)
+    recend = time.perf_counter() - recstart
+    end = time.perf_counter() - start
+    return "Total API endpoint execution time: "+str(end)+"<br>"+"Data processing time: "+str(dataprocessend)+"<br>"+"Recommendation execution time: "+str(recend)+"<br>"+string
